21083db00984087e087e742116ca59dc066d7b3c
simone mwangi <mwanjiis4@gmail.com>
2022-03-15 13:21:23 +0300

    Printing -putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,19 @@
 #include "main.h"
-
 /**
- * prints the letter on the standard output
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
