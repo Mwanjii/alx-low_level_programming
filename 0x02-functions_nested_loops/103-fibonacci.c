fe3a711f474874d797c3ed5bfe1f19222504cf56
simone mwangi <mwanjiis4@gmail.com>
2022-03-15 13:53:33 +0300

    Betty coding style

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,5 +1,11 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
+
+/**
+ * main - finds and prints the sum of the even-valued terms
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
